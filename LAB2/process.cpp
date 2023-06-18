c74d5bc1611506417d917207d235a3fe76abed66
Ksenia Chernyshova <95574685+xenia155@users.noreply.github.com>
2023-06-18 19:17:43 +0300

    Add files via upload

---
+++ LAB2/process.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <conio.h>
+using namespace std;
+
+int main()
+{
+	STARTUPINFO si;
+
+	// Структура piApp типа PROCESS_INFORMATION содержит идентификаторы и дескрипторы нового создаваемого процесса и его главного потока. 
+	PROCESS_INFORMATION piApp; 
+
+	//устанавливаем атрибуты нового процесса
+	ZeroMemory(&si, sizeof(STARTUPINFO)); // все поля структуры si типа STARTUPINFO заполняются нулями.
+	si.cb = sizeof(STARTUPINFO);
+
+	si.dwFlags = STARTF_USEFILLATTRIBUTE; // этот элемент является исходным текстом и цветом фона, если в консольном приложении создается новое окно консоли. В противном случае этот элемент игнорируется.
+	si.dwFillAttribute = BACKGROUND_GREEN;
+	//si.dwFillAttribute = FOREGROUND_GREEN;
+
+	wchar_t args[] = L"12 50 67 23 45 10 78 17";
+
+	// запускаем новый консольный процесс
+	CreateProcess(L"processChild.exe", args, NULL, NULL, FALSE, CREATE_NEW_CONSOLE, NULL, NULL, &si, &piApp);
+	_cputs("The new process is created.\n");
+
+	// ждем закрытия процесса
+	WaitForSingleObject(piApp.hProcess, INFINITE);
+
+	// Мы не используем эти дескрипторы в нашей программе и поэтому закрываем их
+	CloseHandle(piApp.hThread);
+	CloseHandle(piApp.hProcess);
+}
---
+++ LAB2/processChild.cpp
@@ -0,0 +1,71 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <conio.h>
+using namespace std;
+
+void swap(int arr[], int pos1, int pos2) {
+	int temp;
+	temp = arr[pos1];
+	arr[pos1] = arr[pos2];
+	arr[pos2] = temp;
+}
+
+int partition(int arr[], int low, int high, int pivot) {
+	int i = low;
+	int j = low;
+	while (i <= high) {
+		if (arr[i] > pivot) {
+			i++;
+		}
+		else {
+			swap(arr, i, j);
+			i++;
+			j++;
+		}
+	}
+	return j - 1;
+}
+
+void quickSort(int arr[], int low, int high) {
+	if (low < high) {
+		int pivot = arr[high];
+		int pos = partition(arr, low, high, pivot);
+
+		quickSort(arr, low, pos - 1);
+		quickSort(arr, pos + 1, high);
+	}
+}
+
+int main(int args, char* argv[])
+{
+	//int color = atoi(argv[0]);
+	//HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+	//SetConsoleTextAttribute(hConsole, color);//устанавливает цвет текста
+
+	cout << "Mas: ";//вывод массива
+	int* mas = new int[args - 1];
+	for (int i = 0; i < args - 1; i++) {
+		mas[i] = atoi(argv[i]);
+		cout << mas[i] << " ";
+	}
+	cout << endl;
+
+	/*int j = 0;//ищет числа, кратные 5, и переставляет их
+	for (int i = 0; i < args - 1; i++) {
+		if (mas[i] % 5 == 0) {
+			int byf = mas[i];
+			mas[i] = mas[j];
+			mas[j] = byf;
+			j++;
+		}
+	}*/
+	quickSort(mas, 0, args - 2);
+
+	cout << "New formed mas: ";//вывод массива
+	for (int i = 0; i < args - 1; i++) {
+		cout << mas[i] << " ";
+	}
+
+	int a;
+	cin >> a;
+}
